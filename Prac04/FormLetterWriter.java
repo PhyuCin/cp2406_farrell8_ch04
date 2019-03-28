460ee7a3ad6adaf1dc88347b8e8993a4e6a8ddce
PhyuCin <ayemyaphyucin@my.jcu.edu.au>
2019-03-28 20:51:12 +0800

    update FormLetterWriter

--- Prac04/FormLetterWriter.java
+++ Prac04/FormLetterWriter.java
@@ -1,17 +1,23 @@
 public class FormLetterWriter {
 
     public static void displaySalutation(String lastName){
-        System.out.println("\nDear Mr. or Ms. " + lastName + ",\nThank you for your recent order.");
+        System.out.println("\nDear Mr. or Ms. " + lastName + ",");
     }
 
     public static void displaySalutation(String firstName, String lastName){
-        System.out.println("\nDear " + firstName + " " + lastName + ",\nThank you for your recent order.");
+        System.out.println("\nDear " + firstName + " " + lastName + ",");
+    }
+
+    public static void displayLetter(){
+        System.out.println("Thank you for your recent order.");
     }
 
     public static void main(String[] args){
         String firstName = "Harry";
         String lastName = "Potter";
         displaySalutation(firstName, lastName);
+        displayLetter();
         displaySalutation(lastName);
+        displayLetter();
     }
 }
